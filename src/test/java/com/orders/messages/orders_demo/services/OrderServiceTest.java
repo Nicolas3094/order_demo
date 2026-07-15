b55b00b6420cdb86172a3ee2a025c0da396bddaf
Nicolas3094 <nicoo.garcia94@gmail.com>
2026-07-15 16:50:08 -0600

    feat: Create OrderService Unit Test
    	- Covered all cases for OrderService.
    
    	Signed-off-by: Nicolas3094 <nicoo.garcia94@gmail.com>
    
    	# Please enter the commit message for your changes. Lines starting
    	# with '#' will be ignored, and an empty message aborts the commit.
    	#
    	# Date:      Wed Jul 15 16:50:08 2026 -0600
    	#
    	# On branch main

---
+++ src/test/java/com/orders/messages/orders_demo/services/OrderServiceTest.java
@@ -0,0 +1,128 @@
+package com.orders.messages.orders_demo.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.orders.messages.orders_demo.entity.Customer;
+import com.orders.messages.orders_demo.entity.Order;
+import com.orders.messages.orders_demo.enums.OrderStatus;
+import com.orders.messages.orders_demo.exceptions.OrderNotFoundException;
+import com.orders.messages.orders_demo.repositories.OrderRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderServiceTest {
+
+    private Order fakeOrder;
+    private UUID orderId;
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    @InjectMocks
+    private OrderService orderService;
+
+    @BeforeEach
+    public void setup() {
+        fakeOrder = new Order(new Customer("email", "name"), "mxn", new BigDecimal(12341.2131));
+        orderId = fakeOrder.getId();
+    }
+
+    @Test
+    public void getOrder_WhenOrderExists_ReturnsOrder() {
+        when(orderRepository.findById(orderId)).thenReturn(Optional.of(fakeOrder));
+
+        Order result = orderService.getOrder(orderId);
+
+        assertEquals(orderId, result.getId());
+
+    }
+
+    @Test
+    public void getOrder_WhenOrderDoesNotExist_ThrowsOrderNotFoundException() {
+        assertThrows(OrderNotFoundException.class, () -> orderService.getOrder(orderId));
+    }
+
+    @Test
+    public void createOrder_ShouldSaveOrder() {
+
+        orderService.createOrder(fakeOrder);
+
+        verify(orderRepository).save(fakeOrder);
+
+    }
+
+    @Test
+    public void cancelOrder_WhenOrderExists_ShouldCancelOrder() {
+        when(orderRepository.findById(orderId)).thenReturn(Optional.of(fakeOrder));
+
+        orderService.cancelOrder(orderId);
+
+        assertEquals(OrderStatus.CANCELLED, fakeOrder.getStatus());
+        verify(orderRepository).save(fakeOrder);
+    }
+
+    @Test
+    public void cancelOrder_WhenOrderDoesNotExist_ShouldThrowOrderNotFoundException() {
+        assertThrows(OrderNotFoundException.class, () -> orderService.cancelOrder(orderId));
+    }
+
+    @Test
+    public void payOrder_WhenOrderExists_ShouldMarkOrderAsPaid() {
+        when(orderRepository.findById(orderId)).thenReturn(Optional.of(fakeOrder));
+
+        orderService.payOrder(orderId);
+
+        assertEquals(OrderStatus.PAID, fakeOrder.getStatus());
+        verify(orderRepository).save(fakeOrder);
+    }
+
+    @Test
+    public void payOrder_WhenOrderDoesNotExist_ShouldThrowOrderNotFoundException() {
+        assertThrows(OrderNotFoundException.class, () -> orderService.payOrder(orderId));
+    }
+
+    @Test
+    public void expireOrder_WhenOrderExists_ShouldExpireOrder() {
+        when(orderRepository.findById(orderId)).thenReturn(Optional.of(fakeOrder));
+
+        orderService.expireOrder(orderId);
+
+        assertEquals(OrderStatus.EXPIRED, fakeOrder.getStatus());
+        verify(orderRepository).save(fakeOrder);
+    }
+
+    @Test
+    public void expireOrder_WhenOrderDoesNotExist_ShouldThrowOrderNotFoundException() {
+        assertThrows(OrderNotFoundException.class, () -> orderService.expireOrder(orderId));
+    }
+
+    @Test
+    public void refundOrder_WhenOrderExists_ShouldRefundOrder() {
+        fakeOrder.markAsPaid();
+        when(orderRepository.findById(orderId)).thenReturn(Optional.of(fakeOrder));
+
+        orderService.refundOrder(orderId);
+
+        assertEquals(OrderStatus.REFUNDED, fakeOrder.getStatus());
+        verify(orderRepository).save(fakeOrder);
+    }
+
+    @Test
+    public void refundOrder_WhenOrderDoesNotExist_ShouldThrowOrderNotFoundException() {
+        assertThrows(OrderNotFoundException.class, () -> orderService.refundOrder(orderId));
+    }
+
+}
