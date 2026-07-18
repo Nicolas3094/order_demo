3d8e81130589c5f4373c2a0dd96eb001385800db
Jose Nicolas Agustin Garcia Martinez <nicoo.garcia94@gmail.com>
2026-07-17 23:45:35 -0600

    feat: create OrderControllerTest
    Signed-off-by: Jose Nicolas Agustin Garcia Martinez <nicoo.garcia94@gmail.com>

---
+++ src/test/java/com/orders/messages/orders_demo/controllers/OrderControllerTest.java
@@ -0,0 +1,142 @@
+package com.orders.messages.orders_demo.controllers;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.when;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.orders.messages.orders_demo.entity.Customer;
+import com.orders.messages.orders_demo.entity.Order;
+import com.orders.messages.orders_demo.enums.CustomerStatus;
+import com.orders.messages.orders_demo.exceptions.orders.OrderNotFoundException;
+import com.orders.messages.orders_demo.services.OrderService;
+
+@WebMvcTest(OrderController.class)
+public class OrderControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockitoBean
+    private OrderService orderService;
+
+    private Customer customer;
+
+    @Test
+    public void getOrder_ShouldReturn200() throws Exception {
+        UUID customerId = UUID.randomUUID();
+        UUID orderId = UUID.randomUUID();
+        Order order = new Order(
+                customer = new Customer(customerId, "user_email", "user_name", CustomerStatus.ACTIVE, Instant.now()),
+                "MXN",
+                new BigDecimal(123));
+        when(orderService.getOrder(orderId)).thenReturn(order);
+
+        mvc.perform(get("/api/v1/orders/{id}", orderId)).andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.customerId").value(customer.getId().toString()))
+                .andExpect(jsonPath("$.currency").value("MXN"))
+                .andExpect(jsonPath("$.amountTotal").value(123.00))
+                .andExpect(jsonPath("$.status").value("PENDING_PAYMENT"));
+    }
+
+    @Test
+    void getOrder_WhenOrderNotFound_ShouldReturn404() throws Exception {
+        UUID orderId = UUID.randomUUID();
+        when(orderService.getOrder(orderId))
+                .thenThrow(new OrderNotFoundException("Order not found."));
+
+        mvc.perform(get("/api/v1/orders/{id}", orderId))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.status").value(404))
+                .andExpect(jsonPath("$.error").value("Not Found"))
+                .andExpect(jsonPath("$.message").value("Order not found."))
+                .andExpect(jsonPath("$.path").value("/api/v1/orders/" + orderId));
+    }
+
+    @Test
+    public void createOrder_ShouldReturn201() {
+
+    }
+
+    @Test
+    public void createOrder_ShouldReturn400_WhenValidationFails() {
+
+    }
+
+    @Test
+    public void createOrder_ShouldReturn404_WhenCustomerDoesNotExist() {
+
+    }
+
+    @Test
+    public void cancelOrder_ShouldReturn200() {
+
+    }
+
+    @Test
+    public void cancelOrder_ShouldReturn404() {
+
+    }
+
+    @Test
+    public void cancelOrder_ShouldReturn409() {
+
+    }
+
+    @Test
+    public void payOrder_ShouldReturn200() {
+
+    }
+
+    @Test
+    public void payOrder_ShouldReturn404() {
+
+    }
+
+    @Test
+    public void payOrder_ShouldReturn409() {
+
+    }
+
+    @Test
+    public void refundOrder_ShouldReturn200() {
+
+    }
+
+    @Test
+    public void refundOrder_ShouldReturn404() {
+
+    }
+
+    @Test
+    public void refundOrder_ShouldReturn409() {
+
+    }
+
+    @Test
+    public void expireOrder_ShouldReturn200() {
+
+    }
+
+    @Test
+    public void expireOrder_ShouldReturn404() {
+
+    }
+
+    @Test
+    public void expireOrder_ShouldReturn409() {
+
+    }
+
+}
