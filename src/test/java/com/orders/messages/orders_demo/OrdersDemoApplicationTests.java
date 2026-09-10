88d392fc046412788c9248e5d22ee52da9afaa9c
Nicolas3094 <nicoo.garcia94@gmail.com>
2026-09-09 21:14:58 -0600

    test: configure Testcontainers with PostgreSQL integration tests

--- src/test/java/com/orders/messages/orders_demo/OrdersDemoApplicationTests.java
+++ src/test/java/com/orders/messages/orders_demo/OrdersDemoApplicationTests.java
@@ -2,12 +2,14 @@ package com.orders.messages.orders_demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class OrdersDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
